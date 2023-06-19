66eebc4026a83a98a67bbea9f736e044c05c9697
Lugabe <luis2014gbp@gmail.com>
2023-06-19 20:51:18 -0300

    bug corrigido de valor minimo maior do que o maximo

--- script.js
+++ script.js
@@ -3,18 +3,15 @@ const buttonSort = document.querySelector(".button-sort")
 const result = document.querySelector(".h5-sortear")
 
 function sort() {
-    const numeroDe = document.querySelector(".numero-de").value;
-    const numeroAte = document.querySelector(".numero-ate").value;
+    const numeroDe = Math.ceil(document.querySelector(".numero-de").value);
+    const numeroAte = Math.floor(document.querySelector(".numero-ate").value);
+    const valorSorteado = Math.floor(Math.random() * (numeroAte - numeroDe + 1)) + numeroDe;
 
-    const valorSorteado = Math.ceil(Math.random() * (numeroAte - numeroDe + 1));
-
-    if (valorSorteado >= numeroDe) {
+    if (numeroDe >= numeroAte) {
+        result.innerHTML = "O número inicial precisa ser menor do que o numero final!"
+    } else {
         result.innerHTML = "O valor sortedo foi: " + "<b>" + valorSorteado + "</b>";
     }
-    else {
-        sort();
-    }
-
 }
 
 buttonSort.addEventListener("click", sort)
